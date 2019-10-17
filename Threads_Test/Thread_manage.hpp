24d7391ba52fd27a153d41f45e2f2eaba16a3e32
MadHuslista <nicolaschiappacasse@gmail.com>
2019-10-17 04:04:10 -0300

    Comprensión de cómo usar los threads en el proyecto

---
+++ Threads_Test/Thread_manage.hpp
@@ -0,0 +1,18 @@
+#ifndef Thread_manage
+#define Thread_manage
+
+#include <Arduino.h>
+
+extern int LED_1;
+extern int LED_2;
+
+void callthread_1();
+
+
+void callthread_2();
+
+
+
+
+
+#endif /* Thread_manage */
--- Threads_Test/Threads_Test.ino
+++ Threads_Test/Threads_Test.ino
@@ -1,3 +1,4 @@
+#include "Thread_manage.hpp"
 #include <Thread.h>
 #include <ThreadController.h>
 
@@ -6,32 +7,7 @@ ThreadController controll = ThreadController();
 Thread Thread_1 = Thread();
 Thread Thread_2 = Thread(); 
 
-int LED_1 = 7;
-int LED_2 = 3;
 
-void callthread_1()
-{
-  static bool ledstatus = false;
-  ledstatus = !ledstatus;
-
-  digitalWrite(LED_1, ledstatus);
-
-  Serial.print("COOL! I0m running on: ");
-  Serial.print(millis());
-  Serial.println("This is thread_1");
-}
-
-void callthread_2()
-{
-  static bool ledstatus = false;
-  ledstatus = !ledstatus;
-
-  digitalWrite(LED_2, ledstatus);
-
-  Serial.print("COOL! I0m running on: ");
-  Serial.print(millis());
-  Serial.println("This is thread_2");
-}
 
 
 void setup() {
@@ -42,7 +18,7 @@ pinMode(LED_1, OUTPUT);
 pinMode(LED_2, OUTPUT);
 
 Thread_1.onRun(callthread_1);
-Thread_1.setInterval(500);
+Thread_1.setInterval(1000);
 
 Thread_2.onRun(callthread_2);
 Thread_2.setInterval(300);
